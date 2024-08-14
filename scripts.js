1acdc48393495756334b64b2c41e1a30f1a54a04
Marcus Jeong <marcus.jeong96@gmail.com>
2024-08-14 17:23:38 -0400

    added styling and changed JS so that multiple timers can be set within the same window

--- scripts.js
+++ scripts.js
@@ -3,96 +3,184 @@ window.onload = pageLoad;
 //create a page load function
 function pageLoad() {
     //create variables for various HTML elements
-    const countdownTimer = document.getElementById("countdown-container");
-    const countdownMsg = document.getElementById("countdown-message");
+    const countdownTimer1 = document.getElementById("countdown-container1");
+    const countdownMsg1 = document.getElementById("countdown-message1");
+    const countdownTimer2 = document.getElementById("countdown-container2");
+    const countdownMsg2 = document.getElementById("countdown-message2");
     const alarm1 = document.getElementById("alarm-1");
+    const addTimer2 = document.getElementById("addtimer2");
+    const timer2 = document.getElementById("timer-2");
     //buttons
-    const start = document.getElementById("btnStart");
-    const stop = document.getElementById("btnStop");
-    const reset = document.getElementById("resetBtn");
+    const start1 = document.getElementById("btnStart1");
+    const stop1 = document.getElementById("btnStop1");
+    const reset1 = document.getElementById("resetBtn1");
+    const start2 = document.getElementById("btnStart2");
+    const stop2 = document.getElementById("btnStop2");
+    const reset2 = document.getElementById("resetBtn2");
     //time elemeents
-    const minutes = document.getElementById("minutesLeft");
-    const seconds = document.getElementById("secondsLeft");
+    const minutes1 = document.getElementById("minutesLeft1");
+    const seconds1 = document.getElementById("secondsLeft1");
+    const minutes2 = document.getElementById("minutesLeft2");
+    const seconds2 = document.getElementById("secondsLeft2");
 
     //save data to an object
-    const timer1 = {
+    const timer1OBJ = {
         timer:0,
         minute: 0,
         second: 0,
         alarmSet: false,
         isPaused:true
-    }
+    };
+
+    const timer2OBJ = {
+        timer:0,
+        minute: 0,
+        second: 0,
+        alarmSet: false,
+        isPaused:true
+    };
 
     //when start is clicked get the int variable from the document
-    minutes.addEventListener("change", e => {
-        timer1.minute = e.target.value;
+    minutes1.addEventListener("change", e => {
+        timer1OBJ.minute = e.target.value;
     });
-    seconds.addEventListener("change", e => {
-        timer1.second = e.target.value;
+    seconds1.addEventListener("change", e => {
+        timer1OBJ.second = e.target.value;
+    });
+    minutes2.addEventListener("change", e => {
+        timer2OBJ.minute = e.target.value;
+    });
+    seconds2.addEventListener("change", e => {
+        timer2OBJ.second = e.target.value;
     });
 
     //create a function that reduces the time every second
-    function countdown () {
-        if (timer1.timer > 0 && timer1.isPaused === false) {
-            timer1.timer -= 1000;
-            timer1.second--;
-           if (timer1.minute === 0 && timer1.second === 0) {
-                countdownMsg.innerHTML = `0 mins 0 secs`;
-            } else if (timer1.second <= 0) {
-                timer1.minute--;
-                timer1.second = 60;
-            } 
-            if (timer1.minute < 10 && timer1.second < 10) {
-                countdownMsg.innerHTML = `0${timer1.minute} mins 0${timer1.second} secs`;
-            } else if (timer1.minute < 10 && timer1.second >= 10) {
-                countdownMsg.innerHTML = `0${timer1.minute} mins ${timer1.second} secs`;
-            } else {
-                countdownMsg.innerHTML = `${timer1.minute} mins ${timer1.second} secs`;
-            }
-            return timer1.timer;
-        } 
-        
-        if (timer1.timer === 0 && timer1.alarmSet) {
-            minutes.innerHTML = 0;
-            seconds.innerHTML = 0;
-            alarm1.loop = false;
-            timer1.alarmSet = false;
-            timer1.isPaused = true;
-            alarm1.play();
-        }
-    }
-    
-    setInterval(countdown, 1000);
+    //entry
+    function countdown(entry) {
+        let temp;
+        let foo;
+        let bar;
 
+        if (entry === timer1OBJ) {
+            temp = countdownMsg1;
+            foo = minutes1;
+            bar = seconds1;
+        } else {
+            temp = countdownMsg2;
+            foo = minutes2;
+            bar = seconds2;
+        };
+
+        if (entry.timer > 0 && entry.isPaused === false) {
+            entry.timer -= 1000;
+            entry.second--;
+           if (entry.minute === 0 && entry.second === 0) {
+            temp.innerHTML = `0 mins 0 secs`;
+            } else if (entry.second <= 0) {
+                entry.minute--;
+                entry.second = 60;
+            } 
+            if (entry.minute < 10 && entry.second < 10) {
+                temp.innerHTML = `0${entry.minute} mins 0${entry.second} secs`;
+            } else if (entry.minute < 10 && entry.second >= 10) {
+                temp.innerHTML = `0${entry.minute} mins ${entry.second} secs`;
+            } else {
+                temp.innerHTML = `${entry.minute} mins ${entry.second} secs`;
+            }
+            return entry.timer;
+        };
+        
+        if (entry.timer === 0 && entry.alarmSet) {
+            foo.innerHTML = 0;
+            bar.innerHTML = 0;
+            alarm1.loop = false;
+            entry.alarmSet = false;
+            entry.isPaused = true;
+            alarm1.play();
+        };
+    };
+    
     //create a function to start the clock
-    const startClock = () => {
-        countdownTimer.style.display = "flex";
-        let milliMins = timer1.minute * 60000;
-        let milliSecs = timer1.second * 1000;
-        if (timer1.timer === 0) {
-            timer1.timer = milliMins + milliSecs;
-        }
-        timer1.isPaused = false;
-        timer1.alarmSet = true;
+    const startClock = (entry) => {
+        let temp;
+        if (entry === timer1OBJ) {
+            temp = countdownTimer1;
+        } else {
+            temp = countdownTimer2
+        };
+        temp.style.display = "flex";
+        let milliMins = entry.minute * 60000;
+        let milliSecs = entry.second * 1000;
+        if (entry.timer === 0) {
+            entry.timer = milliMins + milliSecs;
+        };
+        entry.isPaused = false;
+        entry.alarmSet = true;
     };
 
-    function resetTimer() {
-        countdownTimer.style.display = "none";
-        timer1.timer = 0;
-        timer1.minute = 0;
-        timer1.second = 0;
-        timer1.isPaused = true;
-        timer1.alarmSet = false;
-    }
+    function resetTimer(entry) {
+        let temp;
+        let foo;
+        let bar;
+        if (entry === timer1OBJ) {
+            temp = countdownTimer1;
+            foo = minutes1;
+            bar = seconds1;
+        } else {
+            temp = countdownTimer2;
+            foo = minutes2;
+            bar = seconds2;
+        };
+        temp.style.display = "none";
+        entry.timer = 0;
+        entry.minute = 0;
+        entry.second = 0;
+        entry.isPaused = true;
+        entry.alarmSet = false;
+        foo.value = "0";
+        bar.value = "0";
+    };
 
-    //event handlers
-    start.onclick = startClock;
-    stop.addEventListener("click", e => {
-        timer1.isPaused = true;
+    function addTimer() {
+        addTimer2.style.display = "none";
+        timer2.style.display = "block";
+    };
+
+    //timer1 event handlers
+    start1.addEventListener("click", e => {
+        startClock(timer1OBJ);
+        setInterval(function() {
+            countdown(timer1OBJ)
+        }, 1000);
+    });
+    stop1.addEventListener("click", e => {
+        timer1OBJ.isPaused = true;
         if (alarm1.duration < 5.77 && alarm1.duration > 0) {
             alarm1.pause();
         }
     });
-    reset.onclick = resetTimer;
+    reset1.addEventListener("click", e => {
+        resetTimer(timer1OBJ);
+    });
+
+    //add second timer
+    addTimer2.onclick = addTimer;
+
+    //timer 2 event handlers
+    start2.addEventListener("click", e => {
+        startClock(timer2OBJ);
+        setInterval(function() {
+            countdown(timer2OBJ)
+        }, 1000);
+    });
+    stop2.addEventListener("click", e => {
+        timer2OBJ.isPaused = true;
+        if (alarm1.duration < 5.77 && alarm1.duration > 0) {
+            alarm1.pause();
+        }
+    });
+    reset2.addEventListener("click", e => {
+        resetTimer(timer2OBJ);
+    });
 }
     
